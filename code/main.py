b60e8f67442ba00d633fb4c781213460c372c0e3
balaklava <zakharova.kz@gmail.com>
2016-09-09 21:16:42 +0200

    classes

--- code/main.py
+++ code/main.py
@@ -1,6 +1,13 @@
-def main():
-    pass
+import sys
+sys.path.append("/home/olyaza/projects/earthquakes/code/")
+import catalogs
 
+
+def main():
+	path_file = "../data/usgs_mmin3_19960101-20160907_lat24.6-50_lon-125--65_us.csv"
+    usgs = catalogs.catalog_USGS()
+    usgs.test_function()
+    usgs.read_catalog()
 
 
 if __name__ == "__main__":
